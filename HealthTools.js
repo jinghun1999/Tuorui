9f5f97988fc3161b6c4020843363139960b2f805
userzhangyx <214040212@qq.com>
2016-08-11 15:32:35 +0800

    update HealthTools.js

--- HealthTools.js
+++ HealthTools.js
@@ -11,33 +11,56 @@ import{
     ScrollView,
     Image,
     View,
-    Dimensions,
 } from 'react-native';
 import Head from './app/commonview/Head';
 import Icon from 'react-native-vector-icons/Ionicons';
-var deviceWidth = Dimensions.get('window').width;
 import IconButton from './app/commonview/HomeIcon';
+import Sale from './app/page/Sales/Sale';
 class HealthTools extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    };
     _member() {
         alert('会员管理')
     }
-    _more(){
+
+    _more() {
         alert('没有更多了')
+    }
+    _salesPress() {
+        const { navigator } = this.props;
+        var _this = this;
+        if (navigator) {
+            navigator.push({
+                name: 'Sale',
+                id: 'page',
+                component: Sale,
+                params: {
+                    id: _this.state.id,
+                }
+            })
+        }
     }
     render() {
         return (
             <View style={styles.container}>
                 <Head title="应用服务"/>
                 <View style={{flex:1}}>
-                    <View style={{flexDirection:'row',borderBottomColor:'#666',borderBottomWidth:StyleSheet.hairlineWidth}}>
+                    <View
+                        style={{flexDirection:'row',borderBottomColor:'#666',borderBottomWidth:StyleSheet.hairlineWidth}}>
                         <Icon name={'ios-home-outline'} size={80} style={{marginTop:15,marginLeft:30}}/>
                         <View style={{flexDirection:'column',height:80,marginLeft:30,marginTop:30}}>
                             <Text style={{justifyContent:'center',height:40}}>天津若瑞宠物医院</Text>
                             <Text style={{justifyContent:'center'}}>天津市滨海新区大同路120号</Text>
                         </View>
                     </View>
-                    <View style={{flexDirection:'row',height:50,borderBottomColor: '#666', borderBottomWidth:StyleSheet.hairlineWidth,}}>
-                        <View style={{flex:1,borderRightColor: '#666', borderRightWidth: StyleSheet.hairlineWidth, justifyContent:'center'}}>
+                    <View
+                        style={{flexDirection:'row',height:50,borderBottomColor: '#666', borderBottomWidth:StyleSheet.hairlineWidth,}}>
+                        <View
+                            style={{flex:1,borderRightColor: '#666', borderRightWidth: StyleSheet.hairlineWidth, justifyContent:'center'}}>
                             <Text style={{marginLeft:30,alignItems:'flex-start',}}>会员：123</Text>
                         </View>
                         <View style={{flex:1,justifyContent:'center'}}>
@@ -45,20 +68,29 @@ class HealthTools extends Component {
                         </View>
                     </View>
                 </View>
-                <View style={{flexDirection:'row',marginTop:20}}>
-                    <IconButton text="我的会员" iconName={'ios-people'} iconColor={'#FF88C2'} onPress={this._member.bind(this)}/>
-                    <IconButton text="宠物管理" iconName={'ios-send'} iconColor={'#CCFF33'} onPress={this._more.bind(this)}/>
-                    <IconButton text="我的疫苗" iconName={'ios-star'} iconColor={'#D2B48C'} onPress={this._more.bind(this)}/>
+                <View style={{flexDirection:'row',marginTop:20,borderTopColor:'#666',borderTopWidth:StyleSheet.hairlineWidth}}>
+                    <IconButton text="我的会员" iconName={'ios-people'} iconColor={'#FF88C2'}
+                                onPress={this._member.bind(this)}/>
+                    <IconButton text="宠物管理" iconName={'ios-send'} iconColor={'#CCFF33'}
+                                onPress={this._more.bind(this)}/>
+                    <IconButton text="我的疫苗" iconName={'ios-star'} iconColor={'#D2B48C'}
+                                onPress={this._more.bind(this)}/>
                 </View>
                 <View style={{flexDirection:'row',marginTop:20}}>
-                    <IconButton text="驱虫疫苗" iconName={'ios-medical'} iconColor={'#87CEFA'} onPress={this._more.bind(this)}/>
-                    <IconButton text="商品销售" iconName={'ios-bus'} iconColor={'#9F88FF'} onPress={this._more.bind(this)}/>
-                    <IconButton text="送检查询" iconName={'ios-search'} iconColor={'#778899'} onPress={this._more.bind(this)}/>
+                    <IconButton text="驱虫疫苗" iconName={'ios-medical'} iconColor={'#87CEFA'}
+                                onPress={this._more.bind(this)}/>
+                    <IconButton text="商品销售" iconName={'ios-bus'} iconColor={'#9F88FF'}
+                                onPress={this._salesPress.bind(this)}/>
+                    <IconButton text="送检查询" iconName={'ios-search'} iconColor={'#778899'}
+                                onPress={this._more.bind(this)}/>
                 </View>
                 <View style={{flexDirection:'row',marginTop:20}}>
-                    <IconButton text="分析报表" iconName={'ios-globe'} iconColor={'#5599FF'} onPress={this._more.bind(this)}/>
-                    <IconButton text="美容服务" iconName={'ios-happy'} iconColor={'#FFB7DD'} onPress={this._more.bind(this)}/>
-                    <IconButton text="more" iconName={'ios-more'} iconColor={'#FFA488'} onPress={this._more.bind(this)}/>
+                    <IconButton text="分析报表" iconName={'ios-globe'} iconColor={'#5599FF'}
+                                onPress={this._more.bind(this)}/>
+                    <IconButton text="美容服务" iconName={'ios-happy'} iconColor={'#FFB7DD'}
+                                onPress={this._more.bind(this)}/>
+                    <IconButton text="more" iconName={'ios-more'} iconColor={'#FFA488'}
+                                onPress={this._more.bind(this)}/>
                 </View>
             </View>
             /*<ScrollView key={'scrollView'}
