081a87fd7ab39f5752c8b099a2a8ba80b3e9b43e
MickHe <MickJson@163.com>
2016-09-18 15:34:30 +0800

    网络图片缓存底层配置信息修改

--- android/app/src/main/java/com/demo/MainApplication.java
+++ android/app/src/main/java/com/demo/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.eguma.barcodescanner.BarcodeScannerPackage;
 import java.util.Arrays;
 import java.util.List;
+import com.rnfs.RNFSPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new RCTCameraPackage(),
-          new BarcodeScannerPackage()
+          new BarcodeScannerPackage(),
+          new RNFSPackage()
       );
     }
   };
